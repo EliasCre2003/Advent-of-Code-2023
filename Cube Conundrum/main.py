e9d137f83dc886b91bbc4fb3aab8105aa9c5c51b
Elias Cregård <eliascregard701@gmail.com>
2023-12-02 21:38:59 +0100

    first commit

---
+++ Cube Conundrum/main.py
@@ -0,0 +1,60 @@
+bag_content = {"red": 12, "green": 13, "blue": 14}
+
+lines: list[str] = []
+
+with open("Cube Conundrum/input.txt") as f:
+    lines = f.readlines()
+    
+class Game:
+    def __init__(self, line: str):
+        line = line.split("\n")[0]
+        temp_line = line.split(": ")
+        temp_line[0] = temp_line[0].split(" ")
+        self.id: int = int(temp_line[0][1])
+        temp_line = temp_line[1].split("; ")
+        self.rounds: list[dict[str, int]] = []
+        for round in temp_line:
+            round = round.split(", ")
+            color_dict = {"red": 0, "green": 0, "blue": 0}
+            for color in round:
+                color = color.split(" ")
+                color_dict[color[1]] = int(color[0])
+            self.rounds.append(color_dict)
+
+games: list[Game] = []
+for line in lines:
+    games.append(Game(line))
+
+def part1():
+    total = 0
+    for game in games:
+        game_max = {"red": 0, "green": 0, "blue": 0}
+        keys = game_max.keys()
+        for round in game.rounds:
+            for key in keys:
+                if round[key] > game_max[key]:
+                    game_max[key] = round[key]
+        possible = True
+        for key in keys:
+            if game_max[key] > bag_content[key]:
+                possible = False
+                break
+        if possible:
+            total += game.id
+    print(total)
+
+def part2():
+    total = 0
+    for game in games:
+        game_minimum = {"red": 0, "green": 0, "blue": 0}
+        keys = game_minimum.keys()
+        for round in game.rounds:
+            for key in keys:
+                if round[key] > game_minimum[key]:
+                    game_minimum[key] = round[key]
+        power = 1
+        for key in keys:
+            power *= game_minimum[key]
+        total += power
+    print(total)
+part2()
---
+++ Trebuchet/main.py
@@ -0,0 +1,84 @@
+lines = []
+with open("Trebuchet/input2.txt", 'r') as f:
+    for line in f:
+        lines.append(line)
+
+def part_1():
+    output = 0
+    for line in lines:
+        foundFirst = False
+        first, last = None, None
+        for char in line:
+            n = 0
+            try:
+                n = int(char)
+            except ValueError:
+                continue
+            if foundFirst:
+                last = n
+            else:
+                first = n
+                foundFirst = True
+        if last is None:
+            last = first
+        output += first * 10 + last
+    print(output)
+
+def part_2():
+    output = 0
+    for line in lines:
+        line.lower()
+        foundFirst = False
+        first, last = None, None
+        i = 0
+        while i < len(line):
+            n = 0
+            try:
+                n = int(line[i])
+            except ValueError:
+                n = string_is_digit(line, i)
+                if n is None:
+                    i += 1
+                    continue
+                else:
+                    i += n[1] - 2
+                    n = n[0]
+            if foundFirst:
+                last = n
+            else:
+                first = n
+                foundFirst = True
+            i+=1
+        if last is None:
+            last = first
+        output += first * 10 + last
+    print(output)
+
+# def part_2():
+#     for i in range(len(lines)):
+#         lines[i] = convertLine(lines[i])
+#     part_1()
+
+
+# def convertLine(line: str) -> str:
+#     digit_map = {"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", 
+#                  "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+#     for string in digit_map:
+#         line = line.replace(string, digit_map[string], )
+#     return line
+
+
+def string_is_digit(line: str, index: int) -> tuple[int, int]:
+    digit_list = [[["one", 1], ["two", 2], ["six", 6]], 
+                  [["zero", 0], ["four", 4], ["nine", 9], ["five", 5]], 
+                  [["three", 3], ["seven", 7], ["eight", 8]]]
+    for i in range(3, 6):
+        if index + i >= len(line):
+            return None
+        string = line[index: index+i]
+        for case in digit_list[i-3]:
+            if string == case[0]:
+                return (case[1], i)
+    return None
+
+part_2()
