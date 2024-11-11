bc00fa7a2ed52d37dbe86f02b95e88e8b4468712
Elias Cregård <eliascregard701@gmail.com>
2024-11-11 01:28:21 +0100

    Still stuck on 1 star :(

--- Pipe Maze/main.py
+++ Pipe Maze/main.py
@@ -2,9 +2,11 @@ from math import ceil
 
 class Ground:
 
-    PIPE_TYPES = {'-', '|', 'L', 'J', 'F', '7', 'S'}
-    HORIZONTAL_PIPES = {'-', 'L', 'F', '7'}
-    VERTICAL_PIPES = {'|', 'L', 'F', '7', 'J'}
+    BENDING_PIPES = {'L', 'F', '7', 'J'}
+    HORIZONTAL_PIPES = BENDING_PIPES.union({'-'})
+    VERTICAL_PIPES = BENDING_PIPES.union({'|'})
+    PIPE_TYPES = HORIZONTAL_PIPES.union(VERTICAL_PIPES)
+    
 
 
     def __init__(self, data: list[list[str]]):
@@ -17,6 +19,9 @@ class Ground:
             
     def get_pipe_at(self, coord: tuple[int, int]) -> str:
         return self.data[coord[1]][coord[0]]
+    
+    # def set_pipe_at(self, coord: tuple[int, int], pipe: str) -> None:
+    #     self.data[coord[1]][coord[0]] = pipe
     
     def next_pipe(self, current_coord: tuple[int, int], previous_coord: tuple[int, int]) -> tuple[str, tuple[int, int], tuple[int, int]]:
         current_pipe = self.get_pipe_at(current_coord)
@@ -52,47 +57,125 @@ class Ground:
 
     def calculate_loop(self) -> list[tuple[int, int]]:
         current = previous = self.starting_position()
-        next_pipe: str
-        length: int = 0
+        next_pipe: str = ""
         loop_coords: list[tuple[int, int]] = []
         while next_pipe != "S":
             next_pipe, current, previous = self.next_pipe(current, previous)
             loop_coords.append(current)
-            length += 1
-        return length
+        return loop_coords
     
     def loop_inside_area(self) -> int:
 
+        loop = self.calculate_loop()
+
         def replace_s() -> list[list[tuple[int, int]]]:
+
             data = [row.copy() for row in self.data]
             s_coord = self.starting_position()
-            if self.get_pipe_at(s_coord[0], s_coord[1] + 1) in Ground.VERTICAL_PIPES:
-                ...
 
-        loop = self.calculate_loop()
-        area: int = 0
-        data_without_s = replace_s(self.data)
-        for y, row in enumerate(data_without_s):
-            inside: bool = False
-            last_pipe: str
-            for x, pipe in enumerate(row):
-                if (x, y) in loop:
-                    if not (last_pipe in Ground.HORIZONTAL_PIPES and pipe in Ground.HORIZONTAL_PIPES):
-                        inside = not inside
-                    last_pipe = pipe
+            def determine_pipe() -> str:
+                if self.get_pipe_at((s_coord[0], s_coord[1] + 1)) in Ground.VERTICAL_PIPES:
+                    if self.get_pipe_at((s_coord[0], s_coord[1] - 1)) in Ground.VERTICAL_PIPES:
+                        return '|'
+                    # elif self.get_pipe_at(s_coord[0] - 1, s_coord[1]) and self.get_pipe_at(s_coord[0] + 1, s_coord[1]) in Ground.VERTICAL_PIPES:
+                    #     return '-'
+                    elif self.get_pipe_at((s_coord[0] - 1, s_coord[1])) in Ground.VERTICAL_PIPES:
+                        return '7'
+                    else:
+                        return 'F'
+                elif self.get_pipe_at((s_coord[0], s_coord[1] - 1)) in Ground.VERTICAL_PIPES:
+                    if self.get_pipe_at((s_coord[0] - 1, s_coord[1])):
+                        return 'J'
+                    else:
+                        return 'L'
                 else:
-                    area += inside
+                    return '-'
+                
+            data[s_coord[1]][s_coord[0]] = determine_pipe()
+            return data
+            
+        data_without_s = replace_s()
+        
+        v_inside_coords: set[tuple[int, int]] = set()
+        for y, row in enumerate(data_without_s):
+            intersections: list[int] = []
+            for x, pipe in enumerate(row):
+                last_tile: str = '' if x == 0 else row[x-1]
+                if (x, y) in loop:
+                    if (pipe == '|' or 
+                        (pipe in ('7', 'J') and Ground.horizontal_connection_possible(last_tile, pipe)) or 
+                        (pipe in ('L', 'F')) and len(intersections) == 1):
+                        intersections.append(x)
+                if len(intersections) < 2:
+                    continue
+                for i in range(intersections[0]+1, intersections[1]):
+                    v_inside_coords.add((i, y))
+                    data_without_s[y][i] = 'I'
+                intersections.clear()
+
+        h_inside_coords: set[tuple[int, int]] = set()
+        for x in range(len(data_without_s[0])):
+            intersections: list[int] = []
+            for y in range(len(data_without_s)):
+                last_tile: str = '' if y == 0 else data_without_s[y-1][x]
+                pipe = data_without_s[y][x]
+                if (x, y) in loop:
+                    if (pipe == '-' or 
+                        (pipe in ('L', 'F') and Ground.vertical_connection_possible(last_tile, pipe)) or
+                        (pipe in ('7', 'J') and len(intersections) == 1)):
+                        intersections.append(y)
+                if len(intersections) < 2:
+                    continue
+                for i in range(intersections[0]+1, intersections[1]):
+                    h_inside_coords.add((x, i))
+                intersections.clear()
+
+        for row in data_without_s:
+            print(row)
+        return len(v_inside_coords.intersection(h_inside_coords))
     
+    @staticmethod
+    def horizontal_connection_possible(pipe1: str, pipe2: str) -> bool:
+        """
+        Checks if a left to right connecction is possible
+        """
+        # if Ground.HORIZONTAL_PIPES.union({pipe1, pipe2}) != Ground.HORIZONTAL_PIPES:
+        #     return False
+        # if pipe1 in ('-', 'F', 'L'):
+        #     return pipe2 in ('7', 'J', '-')
+        # if pipe2 in ('-', '7', 'J'):
+        #     return pipe1 in ('F', 'L', '-')
+        # return False
+        return pipe1 in ('-', 'F', 'L') and pipe2 in ('7', 'J', '-')
+    
+    @staticmethod
+    def vertical_connection_possible(pipe1: str, pipe2: str) -> bool:
+        """
+        Checks if a downwards connection is possible
+        """
+        return pipe1 in ('|', '7', 'F') and pipe2 in ('|', 'L', 'J')
+
+        
+        
+        
+
     def loop_length(self) -> int:
         return len(self.calculate_loop())
 
 
+def part1(lines: list[str]) -> int:
+    ground = Ground([list(line.strip()) for line in lines])
+    return ceil(ground.loop_length() / 2)
+
+def part2(lines: list[str]) -> int:
+    ground = Ground([list(line.strip()) for line in lines])
+    return ground.loop_inside_area()
+
 def main():
-    with open("Pipe Maze/input.txt") as f:
+    with open("Pipe Maze/test.txt") as f:
         lines = f.readlines()
 
-    ground = Ground([list(line) for line in lines])
-    print(ceil(ground.loop_length() / 2))
+    print(part2(lines))
 
 if __name__ == "__main__":
     main()
