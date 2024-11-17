91a92e8fd31a504f3e5e7d9a503994c65c21dba7
Elias Cregård <eliascregard701@gmail.com>
2024-11-17 16:38:30 +0100

    Removed unnecessary class and added docs

--- 9. Mirage Maintenance/main.py
+++ 9. Mirage Maintenance/main.py
@@ -1,47 +1,35 @@
-class History:
-    def __init__(self, values: list[int]):
-        self.values: list[int] = values
+from typing import Callable
 
-    @staticmethod
-    def differences(values: list[int]) -> list[int]:
-        return [values[i+1] - values[i] for i in range(len(values) - 1)]
+def differences(values: list[int]) -> list[int]:
+    return [values[i+1] - values[i] for i in range(len(values) - 1)]
 
-    @staticmethod 
-    def _next(values: list[int]) -> int:
-        differences = History.differences(values)
-        if is_all_zeros(differences):
-            return values[-1]
-        return values[-1] + History._next(differences) 
-    
-    @staticmethod
-    def _previous(values: list[int]) -> int:
-        differences = History.differences(values)
-        if is_all_zeros(differences):
-            return values[0]
-        return values[0] - History._previous(differences)
+def predict_next(values: list[int]) -> int:
+    diff = differences(values)
+    if is_all_zeros(diff):
+        return values[-1]
+    return values[-1] + predict_next(diff)
 
-    def predict_next(self) -> int:
-        return History._next(self.values)
-    
-    def predict_previous(self) -> int:
-        return History._previous(self.values)
-
+def predict_previous(values: list[int]) -> int:
+    diff = differences(values)
+    if is_all_zeros(diff):
+        return values[0]
+    return values[0] - predict_previous(diff)
 
 def is_all_zeros(lst: list[int]) -> bool:
     return lst == [0] * len(lst)
 
-def part1(lines: list[str]) -> int:
-    histories = [History([int(value) for value in line.strip().split(" ")]) for line in lines]
-    return sum(history.predict_next() for history in histories)
+def apply_predictions(histories: list[list[int]], prediction_function: Callable[[list[int]], int]) -> list[int]:
+    return [prediction_function(history) for history in histories] if callable(prediction_function) else None
 
-def part2(lines: list[str]) -> int:
-    histories = [History([int(value) for value in line.strip().split(" ")]) for line in lines]
-    return sum(history.predict_previous() for history in histories)
+def parse_histories(lines: list[str]) -> list[int]:
+    return [[int(value) for value in line.strip().split(" ")] for line in lines]
 
 def main():
     with open("9. Mirage Maintenance/input.txt", "r") as f:
         lines = f.readlines()
-    print(part2(lines))
-    
+    histories = parse_histories(lines)
+    for i, prediction_function in enumerate([predict_next, predict_previous]):
+        print(f'Part {i+1}: {sum(apply_predictions(histories, prediction_function))}')
+
 if __name__ == "__main__":
     main()
